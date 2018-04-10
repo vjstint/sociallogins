e6f99c5d18c430dcc1845e202020bac167e07f9f
Vijay Jeevaraj <vj@ztech.io>
2018-04-10 07:23:00 -0500

    Initial commit

---
+++ src/main/java/org/springframework/social/showcase/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,23 @@
+package org.springframework.social.showcase.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setAfterMessagePrefix("REQUEST DATA : ");
+        return filter;
+    }
+
+}
